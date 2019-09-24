c0e6c89a48e9f81b603b9f4ef3439eb1f3c363d9
Harum Helmy <harumendhah@gmail.com>
2019-09-24 17:11:47 -0500

    need to refactor friend serializer

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,10 +2,11 @@ class FriendsController < ApplicationController
 
   def index
     friends = Friend.all 
-    render json: friends.to_json(friend_serializer_options)
+    render json: friends
   end 
 
   def create
+    byebug
     friend = Friend.create(friend_params)
     render json: friend 
   end
@@ -26,29 +27,29 @@ class FriendsController < ApplicationController
   private
 
   def friend_params
-    params.require(:friend).permit(:name, :pronouns, :appreciation, :starred)
+    params.require(:friend).permit(:name, :user_id, :pronouns, :appreciation, :starred)
   end 
 
-  def friend_serializer_options
-    {
-      :include => {
-        :user => {
-          :except => [:password_digest, :email, :created_at, :updated_at]
-        },
-        }, 
-        :interaction => {
-          :except => [:created_at, :updated_at]
-        },
-        :important_date => {
-          :except => [:created_at, :updated_at]
-        }, 
-        :notes => {
-          :except => [:created_at, :updated_at]
-        }
-      },
-      :except => [:user_id, :created_at, :updated_at]
-    }
-  end 
+  # def friend_serializer_options
+  #   {
+  #     :include => {
+  #       :user => {
+  #         :except => [:password_digest, :email, :created_at, :updated_at]
+  #       },
+  #       }, 
+  #       :interaction => {
+  #         :except => [:created_at, :updated_at]
+  #       },
+  #       :important_date => {
+  #         :except => [:created_at, :updated_at]
+  #       }, 
+  #       :notes => {
+  #         :except => [:created_at, :updated_at]
+  #       }
+  #     },
+  #     :except => [:user_id, :created_at, :updated_at]
+  #   }
+  # end 
 
 
 end
